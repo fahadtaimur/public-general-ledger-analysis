d77aae84651e837e8fa24ab85e817fd1d93e50b3
Fahad Taimur <ftaimur1990@gmail.com>
2026-02-21 23:28:36 -0500

    pipeline to write gl files to S3 raw; clean-up

--- run_pipeline.py
+++ run_pipeline.py
@@ -18,20 +18,13 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s: %(m
 
 
 def run():
-    # Retrieve file name and urls
+    # Retrieve Oklahoma general ledger file name and urls
     file_info_metadata = fetch_oklahoma_gl_urls()
-    print(file_info_metadata)
 
-    # Test with a single file
-    file_info = (
-        "LEDGER_FY25_QTR2.csv",
-        "https://data.ok.gov/dataset/dd1ecf41-4abc-4886-ab0f-b84d7662d8d4/resource/b1b031e1-d26d-4f1f-8892-4fcec01031dc/download/ledger_fy25_qtr2.csv",
-    )
-
-    file_name, file_data = fetch_oklahoma_gl_csv_from_url(file_info=file_info)
-
-    # Upload to S3
-    write_csv_object_to_s3(bucket_name=S3_BUCKET_NAME, object_key=f"{S3_RAW_DIR}/{file_name}", file_data=file_data)
+    # Retrieve the files and write to S3
+    for file_info in file_info_metadata:
+        file_name, file_data = fetch_oklahoma_gl_csv_from_url(file_info=file_info)
+        write_csv_object_to_s3(bucket_name=S3_BUCKET_NAME, object_key=f"{S3_RAW_DIR}/{file_name}", file_data=file_data)
 
 
 if __name__ == "__main__":
